98a6faea205b8577099a43d9c6cb2c0572635548
Mikael Gramont <mgramont@gmail.com>
2020-05-14 15:26:50 +0200

    Rendering workshop test from Frontend Masters course

--- pages/index.js
+++ pages/index.js
@@ -1,208 +1,109 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useRef, useEffect, useState } from 'react';
+import * as d3 from 'd3';
+
+const parse = d3.timeParse("%Y%m%d");
+const initialBooks = [
+  {
+    date: parse("20111001"),
+    name: "Harry Potter and the Philosophers Stone",
+    author: "J. K. Rowling",
+    genre: "fantasy",
+    price: 12,
+  }, {
+    date: parse("20121002"),
+    name: "The Pedagogy of Freedom",
+    author: "Bell hooks",
+    genre: "non-fiction",
+    price: 5,
+  }, {
+    date: parse("20120703"),
+    name: "Harry Potter and the Chamber of Secrets",
+    author: "J. K. Rowling",
+    genre: "fantasy",
+    price: 6,
+  }, {
+    date: parse("20120304"),
+    name: "Gilgamesh",
+    author: "Derrek Hines",
+    genre: "poetry",
+    price: 9,
+  },
+];
+
+const width = 800;
+const height = 300;
+const margin = {top: 20, bottom: 20, left: 20, right: 20};
 
 export default function Home() {
+  const h = 400;
+  const vis = useRef();
+  const [ books, setBooks ] = useState(initialBooks);
+  useEffect(() => {
+    const data = books;
+
+    const svg = d3.select(vis.current);
+
+    // scales
+    const xExtent = d3.extent(data, d => d.date);
+    const yExtent = [0, d3.max(data, d => d.price)];
+
+    const xScale = d3.scaleTime()
+      .domain(xExtent)
+      .range([margin.left, width - margin.right]);
+
+    const yScale = d3.scaleLinear()
+      .domain(yExtent)
+      .range([height - margin.bottom, margin.top]);
+
+    const heightScale = d3.scaleLinear()
+      .domain(yExtent)
+      .range([0, height - margin.top - margin.bottom]);
+
+    // create the rectangles
+    const barWidth = 2;
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d) => xScale(d.date) )
+      .attr('width', barWidth)
+      .attr('y', d => yScale(d.price))
+      .attr('height', d => heightScale(d.price))
+      .attr('fill', 'blue');
+
+    // add the axes
+    const xAxis = d3.axisBottom().scale(xScale)
+    svg.append('g').call(xAxis).attr('transform', `translate(0, ${height - margin.bottom})`);
+
+    const yAxis = d3.axisLeft().scale(yScale);
+    svg.append('g').call(yAxis).attr('transform', `translate(${margin.left}, 0)`);
+
+  }, [books]);
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>D3 explorations</title>
         <link rel="icon" href="/favicon.ico" />
+
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <svg ref={vis}></svg>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+          body { margin:20px; }
+          main {
+            width: 100%;
+            min-height: 100%;
+          }
+          svg {
+            width: 100%;
+            height: 400px;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
       `}</style>
     </div>
   )
